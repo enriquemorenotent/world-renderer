0c57d1c4f84a43e566d8ee57d16a3f69f000823a
Enrique Moreno Tent <enriquemorenotent@gmail.com>
2019-08-21 22:05:10 +0200

    Removed EZThread

--- Runtime/Extinction/Renderer/WorldRenderer.cs
+++ Runtime/Extinction/Renderer/WorldRenderer.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
-using DigitalRuby.Threading;
 using Extinction.Config;
 using Extinction.Utils;
 
