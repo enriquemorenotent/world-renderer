d3d81c7ad086b054ea956d3bcf8000481965458e
Enrique Moreno Tent <enriquemorenotent@gmail.com>
2020-04-15 17:16:13 +0200

    Extend ranges

--- Runtime/Extinction/Config/MapRenderConfig.cs
+++ Runtime/Extinction/Config/MapRenderConfig.cs
@@ -5,8 +5,8 @@ namespace Extinction.Config
     [System.Serializable]
     public class MapRenderConfig
     {
-        [Range(2, 20)] public int radius = 2;
-        [Range(2, 30)] public int chunkSize = 10;
-        [Range(2, 20)] public int cacheRadius = 5;
+        [Range(1, 20)] public int radius = 2;
+        [Range(1, 30)] public int chunkSize = 10;
+        [Range(1, 20)] public int cacheRadius = 5;
     }
 }
